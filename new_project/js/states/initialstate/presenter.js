736480e14482d7c442616a7c44046e05bf625d20
Antonio Feitosa <antonio.cfc@gmail.com>
2020-03-04 05:45:13 -0300

    cleaned

--- new_project/js/states/initialstate/presenter.js
+++
@@ -1,7 +0,0 @@
-const stateInitialStatePresenter = () => {
-    debug(`stateInitialStatePresenter()`)
-    const model = stateInitialStateModel()
-    const view = stateInitialStateView(model)
-    // AddEvenListener, innerHTML or Call Another State.
-}
-
