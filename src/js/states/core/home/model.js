bd6aed384c49c772d69b4cb1c73b106a2081dbdd
Antonio Feitosa <antonio.cfc@gmail.com>
2020-12-13 14:06:24 -0300

    Cellular JS v0.8

--- src/js/states/core/home/model.js
+++ src/js/states/core/home/model.js
@@ -1,5 +1,10 @@
 const stateHomeModel = async (message) => {
     debug(`stateHomeModel()`)
+    // Some useful functions
+    info(message)
+    warning(message)
+    error(message)
+
     return message
 }
 
